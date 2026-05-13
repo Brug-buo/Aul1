635626a39b18a832959c95e59a4b29acc80a5d2b
Bruna <bruna.valera.marques@escola.pr.gov.br>
2026-05-13 20:40:38 -0300

    ex1

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 
-const nome = input.question('Qual o seu nome? ');
-const idade = input.question('Qual a sua idade? ');
-const cidade = input.question('Qual a sua cidade? ');
+const input = require ('readline-sync')
 
-console.log(`Olá! Meu nome é ${nome}, tenho ${idade} anos e sou de ${cidade}.`);
+const nome = input.question('Qual o seu nome? ')
+const idade = input.question('Qual a sua idade? ')
+const cidade = input.question('Qual a sua cidade? ')
+
+console.log(`Olá! Meu nome é ${nome}, tenho ${idade} anos e sou de ${cidade}.`)
